234c3bfe5ca36003f0149e8ef9200f43b1e2f774
obearn <obearn@ekito.fr>
2016-02-29 15:09:14 +0100

    Cleanup repo after Rakefile test

--- nowave-landing/config/prod.js
+++
@@ -1,8 +0,0 @@
-var config = {
-	mixpanel: {
-		key:''
-	},
-	googleForms:{
-		key:''
-	}
-};
--- nowave-landing/src/js/config-do-not-edit.js
+++
@@ -1,8 +0,0 @@
-var config = {
-	mixpanel: {
-		key:'018ed1190f6c364a20466cd006e58cc5'
-	},
-	googleForms:{
-		key:''
-	}
-};
